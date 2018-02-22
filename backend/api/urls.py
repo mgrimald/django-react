d6bf505dedaac8a06e4e07730bc8a028a2206df7
mgrimald <mgrimald@student.42.fr>
2018-02-22 01:08:39 +0100

    added CORS request handler + populated database with few samples

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('articles/<int:id>/', views.article, name='ArticleId'),
     path('categories/', views.categories, name='Categories'),
     path('categories/<int:id>/', views.category, name='CategoryId'),
-    path('categories/<int:id>/articles', views.articlesFromCategory, name='ArticlesFromCategory'),
+    path('categories/<int:id>/articles/', views.articlesFromCategory, name='ArticlesFromCategory'),
     #latest modification
 ]
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'api',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +51,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ORIGIN_ALLOW_ALL=True
+CORS_ALLOW_METHODS = (
+    'GET'
+)
 
 ROOT_URLCONF = 'backend.urls'
 
